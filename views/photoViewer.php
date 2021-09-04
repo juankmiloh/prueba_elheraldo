51eb634282ba3b40bfa8743d4726551e18ed1fec
juankmiloh <juankmiloh@hotmail.com>
2021-09-04 18:48:57 -0500

    update

--- views/photoViewer.php
+++ views/photoViewer.php
@@ -4,24 +4,6 @@
     print("Datos usuario --> ".json_encode($userData));
 ?>
 
-<html lang="es">
-<head>
-<title>Descubre a Barranquilla</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-    <link rel="shortcut icon" href="../img/marketing.ico" type="image/vnd.microsoft.icon">
-    <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/estilos.css" rel="stylesheet">
-    <script src="../js/jquery.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../controller/oauth.js"></script>
-</head>
-<body>
-    <br><br><br><br>
-    <?
+<?
         echo $userData['first_name'];
     ?>
-</body>
-</html>
