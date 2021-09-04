c2a49a832bbb0af6f3b2b9240b0359d309428591
juankmiloh <juankmiloh@hotmail.com>
2021-09-04 03:01:16 -0500

    Update repo

--- views/photoViewer.php
+++ views/photoViewer.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
     $logoutURL = '../_oauth/cerrar.php';
-    $userData = $_SESSION['userData']; 
+    $userData = $_SESSION['userData'];
+    print("Datos usuario --> ".json_encode($userData));
 ?>
 
 <html lang="es">
