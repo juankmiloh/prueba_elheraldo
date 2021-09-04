fc4f8bc4cb3c2a88d14207530334a2fef6de3ded
juankmiloh <juankmiloh@hotmail.com>
2021-09-04 18:51:01 -0500

    update

--- views/photoViewer.php
+++ views/photoViewer.php
@@ -2,9 +2,9 @@
     session_start();
     $logoutURL = '../_oauth/cerrar.php';
     $userData = $_SESSION['userData'];
-    print("Datos usuario --> ".json_encode($userData));
+    // print("Datos usuario --> ".json_encode($userData));
 ?>
 
-<?
+<?php
     echo $userData['first_name'];
 ?>
