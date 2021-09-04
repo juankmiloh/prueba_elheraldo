e75b4dd142c458da3d2ff7a46f3407ad26f63c65
juankmiloh <juankmiloh@hotmail.com>
2021-09-04 03:30:53 -0500

    update

--- views/photoViewer.php
+++ views/photoViewer.php
@@ -1,8 +1,6 @@
 <?php
     session_start();
     $logoutURL = '../_oauth/cerrar.php';
-    $userData = $_SESSION['userData'];
-    print("Datos usuario --> ".json_encode($userData));
 ?>
 
 <html lang="es">
@@ -20,11 +18,39 @@
     <script src="../controller/oauth.js"></script>
 </head>
 <body>
-    <?
-        echo 'IP Local: ' . $_SERVER['SERVER_ADDR'];
-    ?>
+<nav class="navbar navbar-dark bg-dark w-100" style="position: absolute; z-index: 10; background: #0a244f !important;">
+        <div class="container-fluid">
+            <!-- <h2 style="color: white;">Descubre a Barranquilla</h2> -->
+            <div class="row w-100" style="border: 0px solid yellow; padding-top: 0.5%; padding-bottom: 0.5%;">
+                <div class="col-sm-12 col-md-6 logo-colombia">
+                    <img class="img-responsive" src="../img/logo-ct-es.webp" height="50">
+                </div>
+                <div class="col-sm-12 col-md-6 login">
+                    <?php
+                        echo '<a href="'.htmlspecialchars($logoutURL).'"><img src="../img/close.png" height="50"></a>';
+                    ?>
+                </div>
+            </div>
+        </div>
+    </nav>
     
+    <!-- <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
+            <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            ...
+        </div>
+    </div>
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button> -->
 
-    
+    <br><br><br><br><br><br><br>
+
+    <?
+        print("Datos usuario --> ".json_encode($_SESSION['userData']));
+    ?>
+
+
 </body>
 </html>
