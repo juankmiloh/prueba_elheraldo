b7fdcf3cb7d454be2de3654104816fcf6d0b6951
juankmiloh <juankmiloh@hotmail.com>
2021-09-05 16:19:53 -0500

    update

--- src/util/bdConnection.php
+++ src/util/bdConnection.php
@@ -1,29 +1,3 @@
 <?php
-class ConnectionBD {
-    private $dbHost     = "localhost"; // Conexion localhost
-    private $dbPort     = 3307; // Conexion localhost
-    // private $dbHost     = "elheraldo.centralus.cloudapp.azure.com"; // Conexion AZURE
-    // private $dbPort     = 3306; // Conexion AZURE
-    private $dbUsername = "root";
-    private $dbPassword = "root";
-    private $dbName     = "elheraldo";
-    
-    function __construct() {
-        if(!isset($this->db)){
-            // Conectar a la BD
-            $conn = new mysqli($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName, $this->dbPort);
-            if($conn->connect_error){
-                echo("Failed to connect with MySQL: " . $conn->connect_error);
-            }else{
-                echo("Conexión a la base de datos con éxito!");
-                $this->db = $conn;
-            }
-        }
-    }
-
-    function getConnection() {
-        return $this->db;
-    }
-}
-$con = new ConnectionBD();
+echo "Clase de conexión!";
 ?>
