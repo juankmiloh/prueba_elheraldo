679114c0ddc6b05f8234be070037e02ac093b302
juankmiloh <juankmiloh@hotmail.com>
2021-09-06 04:48:55 -0500

    update

--- src/service/uploadFile.php
+++ src/service/uploadFile.php
@@ -1,5 +1,6 @@
 <?php
     header("access-control-allow-origin: *");
+    ob_start(); //Linea para permitir enviar flujo de datos por url al redireccionar la pagina 
 
     if ( 0 < $_FILES['file']['error'] ) {
         echo 'Error: ' . $_FILES['file']['error'] . '<br>';
