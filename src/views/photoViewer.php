32e76b82a84003b14d442e0be6d2e888def43280
juankmiloh <juankmiloh@hotmail.com>
2021-09-06 07:20:57 -0500

    Se coloca boton de agregar foto en pantalla de celular

--- src/views/photoViewer.php
+++ src/views/photoViewer.php
@@ -26,30 +26,31 @@
 <body>
     <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark w-100" style="background: #0a244f !important;">
         <div class="row w-100">
-            <div class="col-sm-6 col-md-10 logo-colombia" style="border: 0px solid red;">
+            <div class="col-sm-12 col-md-10 logo-colombia" style="border: 0px solid red;">
                 <img class="img-responsive" src="../assets/logo-ct-es.webp" height="50">
             </div>
             <div class="col-sm-12 col-md-2 logout-md" style="border: 0px solid red; padding: 0;">
                 <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span style="color: white;"><?php echo $userData['first_name'].' '.$userData['last_name'];?></span>
-                            <img src="<?php echo $userData['picture'];?>" alt="Avatar" class="avatar" style="margin-left: 3%;">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                            <li><a class="dropdown-item" href="" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Subir fotos</a></li>
-                            <?php
-                                echo '<li><a class="dropdown-item" href="'.htmlspecialchars($logoutURL).'">Salir</a></li>';
-                            ?>
-                        </ul>
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span style="color: white;"><?php echo $userData['first_name'].' '.$userData['last_name'];?></span>
+                                <img src="<?php echo $userData['picture'];?>" alt="Avatar" class="avatar" style="margin-left: 3%;">
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Subir fotos</a></li>
+                                <?php
+                                    echo '<li><a class="dropdown-item" href="'.htmlspecialchars($logoutURL).'">Salir</a></li>';
+                                ?>
+                            </ul>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-6 col-md-6 logout-sm" style="border: 0px solid yellow;">
+                <a href="" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" class="btn btn-primary">Subir fotos</a>
                 <?php
-                    echo '<a href="'.htmlspecialchars($logoutURL).'"><img src="../assets/close.png" height="50"></a>';
+                    echo '<a href="'.htmlspecialchars($logoutURL).'"><img src="../assets/close.png" height="39"></a>';
                 ?>
             </div>
         </div>
