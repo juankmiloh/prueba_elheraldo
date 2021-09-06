afb089a83c04ab8f7bad508ea4b25c0e3f967258
juankmiloh <juankmiloh@hotmail.com>
2021-09-06 01:07:32 -0500

    update

--- src/service/usuarios.php
+++ src/service/usuarios.php
@@ -1,25 +1,14 @@
 <?php
+require_once './src/util/classConnection.php';
 
 class User {
-    // private $dbHost     = "localhost"; // Conexion localhost
-    // private $dbPort     = 3307; // Conexion localhost
-    private $dbHost     = "elheraldo.centralus.cloudapp.azure.com"; // Conexion AZURE
-    private $dbPort     = 3306; // Conexion localhost
-    private $dbUsername = "root";
-    private $dbPassword = "root";
-    private $dbName     = "elheraldo";
     private $userTbl    = 'users';
     
     function __construct(){
         if(!isset($this->db)){
             // Conectar a la BD
-            $conn = new mysqli($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName, $this->dbPort);
-            if($conn->connect_error){
-                echo("Failed to connect with MySQL: " . $conn->connect_error);
-            }else{
-                // echo("Conexión a la base de datos con éxito!");
-                $this->db = $conn;
-            }
+            $con = new ConnectionBD();
+            $this->db = $con->getConnection();
         }
     }
     
